c86af87a1ada97138aa2153dc3d0c27f2a53abb5
EJboakye <118780277+EJboakye@users.noreply.github.com>
2022-11-21 20:47:48 -0500

    Add files via upload

---
+++ BudgetCalculator/src/main/java/com/example/budgetcalculator/Area.java
@@ -0,0 +1,37 @@
+package com.example.fake;
+
+public class Area {
+    private double percentage;
+    private String areaName;
+    public Area(String areaName, double percentage){
+        this.areaName = areaName;
+        if (percentage > 100 || percentage < 0){
+            throw new IllegalArgumentException("The percentage has to be between 0 and 100.");
+        }
+        else{
+            this.percentage = percentage;
+        }
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        if (percentage >= 100 || percentage <= 0){
+            throw new IllegalArgumentException("The percentage has to be between 0 and 100.");
+        }
+        else{
+            this.percentage = percentage;
+        }
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+}
