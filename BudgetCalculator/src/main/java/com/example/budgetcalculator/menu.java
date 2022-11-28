a704d72f574b954f4d63d4f6fcf10b698e11c136
aeich17 <105758382+aeich17@users.noreply.github.com>
2022-11-27 23:03:24 -0500

    Update menu.java

--- BudgetCalculator/src/main/java/com/example/budgetcalculator/menu.java
+++ BudgetCalculator/src/main/java/com/example/budgetcalculator/menu.java
@@ -1,7 +1,11 @@
 // Menu Class
 // Purpose: Is used to store userTaxRate and userRevenue in an instance of the class. Contains getter and setter methods.
-
+/** Group Members: Ej Boakye, Adam Eichman, Michel Emborsky, Jake Ferner, Hunter Uebelacker 
+* Date: 11/27/2022 * Section: CSC 331 * Purpose: EmployeeController is the controller class for the Employees.fxml scene. 
+* This scene includes buttons, Vbox, textfields, and a choice box. Method changeSceneBtn2 changes the scene to the Area.fxml file. 
+* Throws an exception if file is not found or does not exist. */
 package com.example.budgetcalculator;
+
 
 public class Menu {
     public double userRevenue;
