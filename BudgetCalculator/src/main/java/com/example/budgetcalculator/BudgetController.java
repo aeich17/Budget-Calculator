7d9727c914d28053f732a01c8d02f21eb2165925
aeich17 <105758382+aeich17@users.noreply.github.com>
2022-11-27 20:21:32 -0500

    Update BudgetController.java

--- BudgetCalculator/src/main/java/com/example/budgetcalculator/BudgetController.java
+++ BudgetCalculator/src/main/java/com/example/budgetcalculator/BudgetController.java
@@ -1,25 +1,22 @@
+
 package com.example.budgetcalculator;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.net.URL;
-import java.text.NumberFormat;
-import java.util.ArrayList;
 
-public class AreaController {
+public class BudgetController implements Initializable {
+    private Scene scene;
 
     @FXML
     private Button Business;
@@ -28,111 +25,45 @@ public class AreaController {
     private Button Personal;
 
     @FXML
-    private Button Settings;
+    private Button homeNext;
 
     @FXML
-    private TextField areaNameInput;
+    private TextField userRevenue;
 
     @FXML
-    private Button areaNext;
+    private TextField userTaxRate;
+    
 
-    @FXML
-    private TableView<Area> allTable;
-
-    @FXML
-    private TableColumn<Area, String> areaTable;
-
-    @FXML
-    private TableColumn<Area, Integer> percentTable;
-
-    @FXML
-    private Label percentLabel;
-
-    @FXML
-    private Slider percentSlider;
-
-    @FXML
-    private Label percentLeft;
-
-    @FXML
-    private Button removeArea;
-
-    @FXML
-    private Button addArea;
-
-
-
-    private static final NumberFormat percent = NumberFormat.getPercentInstance();
-
-    private double areaPercentage = 0.00;
-    private double percentLeftFinal = 1.00;
-
-    public void initialize() {
-
-            percentSlider.valueProperty().addListener((ov, oldValue, newValue) -> {
-                areaPercentage = (newValue.intValue() / 100.0);
-                percentLabel.setText(percent.format(areaPercentage));
-            });
-
-
-        areaTable.setCellValueFactory(new PropertyValueFactory<Area, String>("areaName"));
-        percentTable.setCellValueFactory(new PropertyValueFactory<Area, Integer>("percentage"));
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+    double afterTaxRevenue;
+    double revenue;
+    double taxAmount;
+    public void changeSceneBtn1() throws Exception {;
+        double tax, tax1;
 
-    double countPercent = 100;
+        try {
+            revenue = Double.parseDouble(userRevenue.getText());
+            tax = Double.parseDouble(userTaxRate.getText());
+            tax1 = (tax / 100);
+            taxAmount = revenue * tax1;
+            afterTaxRevenue = (revenue - taxAmount);
 
-    ArrayList<String> arrayAreas = new ArrayList<String>();
-
-    // Add
-    @FXML
-    void addAreaAction(ActionEvent event) {   // change to try catch
-        countPercent -= Math.round(areaPercentage * 100);
-
-
-        if ((countPercent <= 100) & (countPercent >= 0)) {
-            Area area = new Area(areaNameInput.getText(), areaPercentage);
-            ObservableList<Area> areas = allTable.getItems();
-            areas.add(area);
-            allTable.setItems(areas);
-            percentLeftFinal = countPercent / 100;
-            arrayAreas.add(String.valueOf(area));
-            percentLeft.setText(percent.format(percentLeftFinal));
-            areaNameInput.clear();
-            percentSlider.setValue(percentSlider.getMin());
+        } catch (NumberFormatException ex) {
+            userRevenue.setText("Enter amount");
+            userTaxRate.setText("Enter percentage");
+            userRevenue.requestFocus();
         }
 
-        else {
-            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
-            errorAlert.setHeaderText("User Error");
-            errorAlert.setContentText("Total percentage must be no more than 100");
-            errorAlert.showAndWait();
-            countPercent += Math.round(areaPercentage * 100);
-            percentSlider.setValue(percentSlider.getMin());
 
-        }
-    }
+        Menu menu = new Menu(revenue, taxAmount, afterTaxRevenue); // change to pass afterTax
 
-    @FXML
-    void removeAreaAction(ActionEvent event) {
-        Area tempArea = allTable.getSelectionModel().getSelectedItem();
-        countPercent += (tempArea.getPercentage()) * 100;
-        percentLeftFinal = countPercent / 100;
-        percentLeft.setText(percent.format(percentLeftFinal));
-        ObservableList<Area> chosenRow = allTable.getSelectionModel().getSelectedItems();
-        ArrayList<Area> rowDelete = new ArrayList<>(chosenRow);
-        arrayAreas.add(String.valueOf(chosenRow));  // Test
-        rowDelete.forEach(row -> allTable.getItems().remove(row));
-    }
-
-
-
-
-    public void changeSceneBtn3() throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Pie.fxml"));
-        Stage window = (Stage) areaNext.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("Employees.fxml"));
+        Stage window = (Stage) homeNext.getScene().getWindow();
         window.setScene(new Scene(root));
 
     }
-}
 
+}
