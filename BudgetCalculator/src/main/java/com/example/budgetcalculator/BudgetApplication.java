763201d245283ae0b761c6df2addcef90ed2945f
aeich17 <105758382+aeich17@users.noreply.github.com>
2022-11-27 22:57:39 -0500

    Update BudgetApplication.java

--- BudgetCalculator/src/main/java/com/example/budgetcalculator/BudgetApplication.java
+++ BudgetCalculator/src/main/java/com/example/budgetcalculator/BudgetApplication.java
@@ -1,3 +1,9 @@
+/** Group Members: Ej Boakye, Adam Eichman, Michel Emborsky, Jake Ferner, Hunter Uebelacker
+* Date: 11/27/2022 
+* Section: CSC 331 
+* Purpose: BudgetApplication acts as a driver file that runs and starts the application. The method start trys to start the first scene which would 
+be the * BudgetApplicationHome.fxml file. Throws an exception if file is not found or does not exist. */
+
 package com.example.budgetcalculator;
 
 import javafx.application.Application;
