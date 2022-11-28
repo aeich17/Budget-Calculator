61abc87424d6f09be9713635d913edb177843232
aeich17 <105758382+aeich17@users.noreply.github.com>
2022-11-27 22:59:19 -0500

    Update Employee.java

--- BudgetCalculator/src/main/java/com/example/budgetcalculator/Employee.java
+++ BudgetCalculator/src/main/java/com/example/budgetcalculator/Employee.java
@@ -1,3 +1,6 @@
+/** Group Members: Ej Boakye, Adam Eichman, Michel Emborsky, Jake Ferner, Hunter Uebelacker * Date: 11/27/2022 
+* Section: CSC 331 * Purpose: The Employee class creates an object called Employee. The object has only one instance variable, 
+* which is name. There is a get and set method for the name instance variable. */
 package com.example.budgetcalculator;
 
 abstract public class Employee {
